ad9c82795182c335b4b96c038d1b8b1c33ffc9e3
nhadd <daoducnha29493@gmail.com>
2020-06-24 14:56:42 +0700

    Finish refactor

--- src/App.js
+++ src/App.js
@@ -2,19 +2,55 @@ import React, { Component } from "react";
 import Header from './components/Header';
 import Map from './components/Map';
 import {Container} from 'reactstrap';
-require('dotenv').config()
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      applicationName:'TrafficMap'
+      applicationName:'TrafficMap',
+      data: null,
+      api_url: "http://data.edmonton.ca/resource/87ck-293k.json",
     };
   }
 
-  toggle() {
-    this.setState({ isOpen: !this.state.isOpen });
+  createFeatureCollection(data) {
+    let features = [];
+
+    data.forEach((element) => {
+      features.push({
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [
+            parseFloat(element.location.longitude),
+            parseFloat(element.location.latitude),
+          ],
+        },
+        properties: {
+          description: element.description,
+          details: element.details,
+          impact: element.impact,
+          duration: element.duration,
+        },
+      });
+    });
+
+    return {
+      type: "FeatureCollection",
+      features: features,
+    };
+  }
+
+  componentDidMount() {
+    const { data, api_url } = this.state;
+
+    if (!data) {
+      fetch(api_url, { method: "GET" })
+        .then((response) => response.json())
+        .then((response) => this.createFeatureCollection(response))
+        .then((response) => this.setState({ data: response }));
+    }
   }
 
   render() {
@@ -22,7 +58,7 @@ class App extends Component {
       <div className="App">
         <Header appName={this.state.applicationName}/>
         <Container>
-          <Map />
+          <Map data={this.state.data} />
         </Container>
         
       </div>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,30 +1,28 @@
 import React, { Component } from "react";
-import { Container } from "reactstrap";
-import MapboxGL, { accessToken } from "mapbox-gl";
+// import { Container } from "reactstrap";
+import MapboxGL from "mapbox-gl";
 
 export default class Map extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      api_url: "http://data.edmonton.ca/resource/87ck-293k.json",
+    this.state = {      
       map: false,
       viewport: {
         zoom: 10,
         latitude: 53.52666097368566,
         longitude: -113.39388775920357,
         center: [-113.39388775920357, 53.52666097368566],
-      },
-      data: null,
+      }      
     };
   }
 
-  initializeMap() {
+  static initializeMap(state, viewport) {
     MapboxGL.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
     let map = new MapboxGL.Map({
       container: "map",
       style: "mapbox://styles/mapbox/streets-v11",
-      ...this.state.viewport,
+      ...viewport,
     });
 
     map.on("load", () => {
@@ -33,7 +31,7 @@ export default class Map extends Component {
         type: "circle",
         source: {
           type: "geojson",
-          data: this.state.data,
+          data: state.data,
         },
         paint: {
           "circle-radius": 5,
@@ -44,67 +42,48 @@ export default class Map extends Component {
 
     map.on("click", "points", (e) => {
       const coordinates = e.features[0].geometry.coordinates.slice();
-      const {details, description, impact, duration} = e.features[0].properties; 
+      const {
+        details,
+        description,
+        impact,
+        duration,
+      } = e.features[0].properties;
 
-      while(Math.abs(e.lngLat.lng - coordinates[0]) >180) {
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
         coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
       }
 
       new MapboxGL.Popup()
-      .setLngLat(coordinates)
-      .setHTML(`
+        .setLngLat(coordinates)
+        .setHTML(
+          `
         <strong>${description}</strong>
         <em>${impact}</em>
         <em>${duration}</em>
         <p>${details}</p>
-      `)
-      .addTo(map);
-    });
-    this.setState({ map });
-  }
-
-  createFeatureCollection(data) {
-    let features = [];
-
-    data.forEach((element) => {
-      features.push({
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [
-            parseFloat(element.location.longitude),
-            parseFloat(element.location.latitude),
-          ],
-        },
-        properties: {
-          description: element.description,
-          details: element.details,
-          impact: element.impact,
-          duration: element.duration,
-        },
-      });
+      `
+        )
+        .addTo(map);
     });
 
-    return {
-      type: "FeatureCollection",
-      features: features,
-    };
+    map.on("mouseenter", "points", () => {
+      map.getCanvas().style.cursor = "";
+    });
+
+    return { map };
   }
 
-  componentDidMount() {
-    const { data, api_url } = this.state;
-
-    if (!data) {
-      fetch(api_url, { method: "GET" })
-        .then((response) => response.json())
-        .then((response) => this.createFeatureCollection(response))
-        .then((response) => this.setState({ data: response }));
-    }
+  static getDerivedStateFromProps(nextProps, prevState) {    
+    const { map, data } = nextProps;
+    if (data && !map) return Map.initializeMap(nextProps, prevState.viewport);
+    else return null;
   }
+
+  
+
+ 
 
   render() {
-    const { map, data } = this.state;
-    if (data && !map) this.initializeMap();
     return <div style={{ width: 1100, height: 600 }} id="map"></div>;
   }
 }
